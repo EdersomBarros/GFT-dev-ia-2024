0a08794a6a8520accd16a24a41f8691fa548f2f7
ederson barros <edersombarros@hotmail.com>
2024-07-04 08:56:19 -0400

    First Commit

---
+++ src/test/java/me/dio/GFT_dev_ia_2024/ApplicationTests.java
@@ -0,0 +1,13 @@
+package me.dio.GFT_dev_ia_2024;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
